80725192b860779c4f841f4f061d5935677c1a29
maybeags <suren0709@gmail.com>
2025-02-03 15:40:05 +0900

    feat: 20250203 making bmi calc

---
+++ src/main/java/chapter04_scanner/Scanner04BmiCalc.java
@@ -0,0 +1,19 @@
+package chapter04_scanner;
+/*
+    Bmi 계산기를 작성합니다.
+
+    1. 키를 cm로 입력 받습니다.
+    2. 몸무게를 kg으로 입력 받습니다.
+    3. bmi를 계산하는 방법 : 몸무게(kg) / (키(m)의 제곱)
+    4. 콘솔창에 "당신의 bmi지수는 xx입니다."
+
+실행 예
+    당신의 키(cm)를 입력하세요 >>> 172.3
+    당신의 몸무게(kg)를 입력하세요 >>> 70
+    당신의 bmi 지수는 xx입니다.
+ */
+public class Scanner04BmiCalc {
+    public static void main(String[] args) {
+
+    }
+}
