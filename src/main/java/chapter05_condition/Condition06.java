c1ba179f8fa5b5ee18f85fa59aca5747ad8d7adb
maybeags <suren0709@gmail.com>
2025-02-04 16:14:08 +0900

    feat: 20250204 loop lesson

--- src/main/java/chapter05_condition/Condition06.java
+++ src/main/java/chapter05_condition/Condition06.java
@@ -42,9 +42,24 @@ public class Condition06 {
         } else {
             // 여기서부터 switch문을 작성
             switch (changedScore) {
-                case
+                case 10:
+                case 9:
+                    grade = "A";
+                    break;
+                case 8:
+                    grade = "B";
+                    break;
+                case 7:
+                    grade = "C";
+                    break;
+                case 6:
+                    grade = "D";
+                    break;
+                default:
+                    grade = "F";
             }
         }
+        System.out.println("당신의 학점은 " + grade + "입니다.");
 
 
 
